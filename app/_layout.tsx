b564f4c5bca5a7cbb9589ca24d00590fa6a0051d
herdeybayor <shereefdeen4real@gmail.com>
2025-06-15 19:41:12 +0100

    Initial commit
    Generated by create-expo-stack 2.18.3

---
+++ app/_layout.tsx
@@ -0,0 +1,28 @@
+import { useUnistyles } from 'react-native-unistyles';
+
+import { Stack } from 'expo-router';
+
+export const unstable_settings = {
+  // Ensure that reloading on `/modal` keeps a back button present.
+  initialRouteName: '(tabs)',
+};
+
+export default function RootLayout() {
+  const { theme } = useUnistyles();
+
+  return (
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: theme.colors.background,
+        },
+        headerTitleStyle: {
+          color: theme.colors.typography,
+        },
+        headerTintColor: theme.colors.typography,
+      }}>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+    </Stack>
+  );
+}
