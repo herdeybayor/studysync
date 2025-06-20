8f3803ec4445ab8d02009c682cde175b23dee5c6
herdeybayor <shereefdeen4real@gmail.com>
2025-06-20 13:37:41 +0100

    Enhance HomeHeader component with improved styling and search functionality
    - Updated greeting and name display with conditional rendering and new styles for better visibility.
    - Adjusted icon sizes for consistency across the header.
    - Added a search bar with updated styles for a more user-friendly interface.
    - Refactored header styles to utilize theme spacing and colors for improved design coherence.

--- components/custom/home-header.tsx
+++ components/custom/home-header.tsx
@@ -11,18 +11,18 @@ export default function HomeHeader({ appSettings }: { appSettings: AppSettings }
     <View style={styles.headerContainer}>
       <View style={styles.header}>
         <View style={styles.headerLeft}>
-          <Text>Welcome back</Text>
-          <Text>{appSettings.firstName} 👋</Text>
+          <Text style={styles.headerLeftGreeting}>Welcome back</Text>
+          <Text style={styles.headerLeftName}>{appSettings.firstName ?? 'N/A'} 👋🏾</Text>
         </View>
         <View style={styles.headerRight}>
           {[
             {
-              icon: <Icons name="scan-outline" size={20} color={theme.colors.primary} />,
+              icon: <Icons name="scan-outline" size={16} color={theme.colors.primary} />,
               label: 'Scan',
               onPress: () => {},
             },
             {
-              icon: <Icons name="notifications-outline" size={20} color={theme.colors.primary} />,
+              icon: <Icons name="notifications-outline" size={16} color={theme.colors.primary} />,
               label: 'Notifications',
               onPress: () => {},
             },
@@ -33,28 +33,68 @@ export default function HomeHeader({ appSettings }: { appSettings: AppSettings }
           ))}
         </View>
       </View>
+
+      <TouchableOpacity>
+        <View style={styles.headerSearch}>
+          <Icons name="search-outline" size={20} color="#667185" />
+          <Text style={styles.headerSearchText}>Search for recording</Text>
+        </View>
+      </TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create((theme) => ({
   headerContainer: {
-    paddingHorizontal: 16,
-    paddingVertical: 12,
+    paddingHorizontal: theme.spacing(3.5),
+    paddingVertical: theme.spacing(8),
+    borderRadius: 36,
+    backgroundColor: theme.colors.primary,
   },
   header: {
     flexDirection: 'row',
     justifyContent: 'space-between',
   },
   headerLeft: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    gap: theme.spacing(0.5),
   },
   headerRight: {
     flexDirection: 'row',
     alignItems: 'center',
+    gap: theme.spacing(2),
   },
   headerRightItem: {
-    marginLeft: 16,
+    backgroundColor: '#E9F2FD',
+    width: theme.spacing(10),
+    height: theme.spacing(10),
+    borderRadius: theme.spacing(10),
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  headerLeftGreeting: {
+    fontSize: 16,
+    fontWeight: 400,
+    color: '#F9FAFB99',
+  },
+  headerLeftName: {
+    fontSize: 20,
+    fontWeight: 700,
+    color: theme.colors.white,
+  },
+  headerSearch: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: theme.spacing(2),
+    backgroundColor: theme.colors.white,
+    padding: theme.spacing(4),
+    borderRadius: theme.spacing(10),
+    marginTop: theme.spacing(7),
+    borderWidth: 1,
+    borderColor: '#D0D5DD',
+  },
+  headerSearchText: {
+    fontSize: 14,
+    fontWeight: 400,
+    color: '#667185',
   },
 }));
